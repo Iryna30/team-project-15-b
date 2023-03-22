6b3ab81939fc265db02ac4969a34355f60d8b857
Iryna30 <boloban1993@gmail.com>
2023-03-22 16:52:13 +0200

    fin

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,21 @@
 const swiper2 = new Swiper('.reviews-swiper', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: '.reviews-swiper-pagination',
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.reviews-swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
     },
-    slidesPerView: 1,
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 16,
-        },
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 28,
-        }
-    }
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
 });
 new Swiper('.image-slider', {
   direction: 'horizontal',
